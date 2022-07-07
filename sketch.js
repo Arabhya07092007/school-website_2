eeb44a38a5d854c930dcc29c6927550a2a1eee3b
Arabhaya <72385709+Arabhya07092007@users.noreply.github.com>
2022-07-07 21:36:15 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "330px";
+  document.getElementById("mySidenav").style.width = "520px";
   document.getElementById("main").style.marginLeft = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
